b91d51f36a7400d0623500862c22701a6d04bbdd
srs-ls <yurakidev@gmail.com>
2020-07-07 20:19:55 -0500

    RPS - versions 1

---
+++ lesson_2/rps.rb
@@ -0,0 +1,48 @@
+VALID_CHOICES = %w(rock paper scissors).freeze
+
+def win?(first, second)
+  (first == "rock" && second == "scissors") ||
+    (first == "paper" && second == "rock") ||
+    (first == "scissors" && second == "paper")
+end
+
+def display_results(player, computer)
+  if win?(player, computer)
+    prompt("You won!")
+  elsif win?(computer, player)
+    prompt("Computer won!")
+  else
+    prompt("It's a tie!")
+  end
+end
+
+def prompt(message)
+  puts("=> #{message}")
+end
+
+loop do
+  choice = ""
+
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    choice = gets.chomp
+
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice")
+    end
+  end
+
+  computer_choice = VALID_CHOICES.sample
+
+  puts("You chose: #{choice}, the computer chose #{computer_choice}")
+
+  display_results(choice, computer_choice)
+
+  prompt("Do you want to play again?")
+  answer = gets.chomp
+  break unless answer.downcase.start_with?("y")
+end
+
+prompt("Thanks you for playing. Good bye!")
